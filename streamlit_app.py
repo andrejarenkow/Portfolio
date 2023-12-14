b674a3a6cb66feecbe0088c4cd127b61ac06b61f
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2023-12-14 17:48:29 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,8 @@ st.set_page_config(
 
 # Barra lateral para info
 with st.sidebar:
-    st.image('https://media.licdn.com/dms/image/D4D03AQEDuEud7TtpUw/profile-displayphoto-shrink_800_800/0/1664902362993?e=1704326400&v=beta&t=-FzZf4tS3p9l4KsfqoCMUAGDiaMUkmLcTHb6d6-UczA', width=100)
+ 
+    st.image('https://media.licdn.com/dms/image/D4D03AQEDuEud7TtpUw/profile-displayphoto-shrink_800_800/0/1664902362993?e=1704326400&v=beta&t=-FzZf4tS3p9l4KsfqoCMUAGDiaMUkmLcTHb6d6-UczA')
     texto = """
 Hello! My name is André Jarenkow, and this is my Portfolio!.
             """
