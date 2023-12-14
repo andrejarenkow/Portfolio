eb5cb4be399542d141cca4d971e939fddd598bba
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2023-12-14 17:47:59 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,19 +21,9 @@ st.set_page_config(
 
 # Barra lateral para info
 with st.sidebar:
-    texto = """
-# Sobre o painel
-Este painel foi feito a partir de um estudo da biblioteca Spotipy, a qual facilita o consumo da API do Spotify. Selecionei o podcast UP! para avaliar seus dados, e deixá-los postos de uma maneira
-que fosse fácil de entender.
-
-# Sobre mim
-            """
-    st.markdown(texto)
     st.image('https://media.licdn.com/dms/image/D4D03AQEDuEud7TtpUw/profile-displayphoto-shrink_800_800/0/1664902362993?e=1704326400&v=beta&t=-FzZf4tS3p9l4KsfqoCMUAGDiaMUkmLcTHb6d6-UczA', width=100)
-
     texto = """
-
-Me chamo André Jarenkow, entusiasta da linguagem Python e ouvinte fiel ao UP!.
+Hello! My name is André Jarenkow, and this is my Portfolio!.
             """
     st.markdown(texto)
 
