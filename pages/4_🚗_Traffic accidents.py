ae68354e4048f92cf4fc1387aa22d467f01cef79
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-01-16 17:31:05 -0300

    Update 4_🚗_Traffic accidents.py

--- pages/4_🚗_Traffic accidents.py
+++ pages/4_🚗_Traffic accidents.py
@@ -172,7 +172,7 @@ acid_mes.update_layout(yaxis_range=[0,acidentes_por_mes['accidents'].max()])
 ##########################
 
 with col3:
-    st.plotly_chart(acid_mes)
+    st.plotly_chart(acid_mes, use_container_width=True)
     #st.plotly_chart(acid_hora, theme=None)
     st.markdown("[Data source](https://dadosabertos.poa.br/dataset/acidentes-de-transito-acidentes)")
 
